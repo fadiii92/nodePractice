4b1aed481179d746a6660a7dc89ad2ee3dc39bb1
fadii72 <163737610+fadiii92@users.noreply.github.com>
2024-10-26 00:50:55 +0500

    handlebars with my own errors

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
-const expressHbs = require('express-handlebars')
+const { create } = require('express-handlebars')
 
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
 
-app.engine('hbs', expressHbs())
-app.set('view engine', 'pug')
+// Configure Handlebars with the correct file extension
+const hbs = create({ extname: '.hbs', defaultLayout: 'main-layout.hbs' })
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -19,10 +20,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
-
-app.use((req,res,next)=>{
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-    res.status(404).render('404', {pageTitle:'404'})
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: '404' })
 })
 
 app.listen(3000)
