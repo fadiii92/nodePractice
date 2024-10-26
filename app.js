bc1fd764de978bc0c5cf414d6d26b5e5e7953d6d
fadii72 <163737610+fadiii92@users.noreply.github.com>
2024-10-26 11:47:33 +0500

    ejs done now deleteing extra

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const { create } = require('express-handlebars'); // updated import
 
 const app = express();
 
-// Create and configure the Handlebars engine
-const hbs = create({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main-layout',
-  extname: 'hbs',
-  helpers: {
-    extend: function(context) {
-      return context; // Adjust this as needed for your logic
-    },
-  },
-});
-
-// Register the engine with express
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -32,9 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' , path : ''});
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+app.listen(3000);
