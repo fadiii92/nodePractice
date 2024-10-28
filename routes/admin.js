11143b8ddb51b1958d5c7e8350c9327c8ff2907c
fadii72 <163737610+fadiii92@users.noreply.github.com>
2024-10-28 23:30:38 +0500

    worked on ejs template and made controllers

--- routes/admin.js
+++ routes/admin.js
@@ -2,28 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsFunc = require('../controlers/products')
 
 const router = express.Router();
 
-const products = [];
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', productsFunc.addProductPage);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsFunc.postProduct);
 
 exports.routes = router;
-exports.products = products;
